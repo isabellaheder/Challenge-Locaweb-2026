bd4d9566e7a11794741cae8a93986aa2101b5f3c
Isabella Heder <isabella.f.heder@gmail.com>
2026-09-11 15:47:14 -0300

    Publica arquivos no bq

--- Job-Pipeline-GCP/publish_bigquery.py
+++ Job-Pipeline-GCP/publish_bigquery.py
@@ -12,13 +12,18 @@ arquivos = {
     "previsao_sla": "outputs/previsao_sla.csv",
     "previsao_turno": "outputs/previsao_incidentes_turno.csv",
     "previsao_duracao": "outputs/export_previsao_duracao.csv",
-    "clusters_incidentes": "outputs/clusters_incidentes.csv"}
+    "clusters_incidentes": "outputs/clusters_incidentes.csv",
+    "cluster_nlp": "outputs/incident_clusters.csv",
+    "previsao_d1_por_cluster": "outputs/previsao_d1_por_cluster.csv",
+    "previsao_d7_por_cluster": "outputs/previsao_d7_por_cluster.csv",
+    "metricas_previsao_por_cluster": "outputs/metricas_previsao_por_cluster.csv",}
 
 for tabela, arquivo in arquivos.items():
     print(f"Publicando {tabela}")
     df = pd.read_csv(arquivo)
-    job = client.load_table_from_dataframe(
-        df, f"{PROJECT_ID}.{DATASET}.{tabela}")
+
+    job_config = bigquery.LoadJobConfig(write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE)
+    job = client.load_table_from_dataframe(df, f"{PROJECT_ID}.{DATASET}.{tabela}", job_config=job_config)
 
     job.result()
     print(f"OK -> {tabela}")
